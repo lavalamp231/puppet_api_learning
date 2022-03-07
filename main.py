91dfe998e198c462748664be646d9a980e4eefb9
root <andrew.w.simons@gmail.com>
2022-03-06 21:57:31 -0600

    test

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import requests
+
+ca_location = '/etc/puppetlabs/puppet/ssl/certs/ca.pem'
+cert = '/etc/puppetlabs/puppet/ssl/certs/canary.esxi.com.pem'
+private_key = '/etc/puppetlabs/puppet/ssl/private_keys/canary.esxi.com.pem'
+
+def get_fact():
+    
